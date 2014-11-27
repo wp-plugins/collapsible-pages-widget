d10c414a5c26f06a14d5be6a0e3d00ff29d610f3
adamschoenemann <adamschoenemann@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-27 09:37:41 +0000

    bumped version nr
    git-svn-id: https://plugins.svn.wordpress.org/collapsible-pages-widget/trunk@1033763 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- collapsible-pages-widget.php
+++ collapsible-pages-widget.php
@@ -3,7 +3,7 @@
  * Plugin Name: Collapsible Pages Widget
  * Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
  * Description: A brief description of the plugin.
- * Version: 0.0.1
+ * Version: 0.1.0
  * Author: Adam Schønemann
  * Author URI: http://URI_Of_The_Plugin_Author
  * License: GPL2
